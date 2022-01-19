1c510879dbb427b46b4fdd84efc988df5b626672
Yonatan Karby <jokerkarby@gmail.com>
2022-01-19 15:20:01 +0200

    added client begin write function

--- PingPongExercise/Client/ClientSocket.cs
+++ PingPongExercise/Client/ClientSocket.cs
@@ -1,13 +1,43 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Client
 {
     public class ClientSocket : ITcpClient
     {
-        public Task BeginWrite()
+        private readonly IPAddress _ip;
+        private readonly int _port;
+        private readonly IPEndPoint _endpoint;
+        private readonly Socket _socket;
+
+        private bool _isRunning = true;
+
+        public ClientSocket(IPAddress ip, int port)
         {
-            throw new NotImplementedException();
+            _ip = ip;
+            _port = port;
+            _endpoint = new IPEndPoint(_ip, _port);
+            _socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        }
+        public async Task BeginWrite()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Connecting...");
+                _socket.Connect(_endpoint);
+                Console.WriteLine("Connected");
+
+                while (_isRunning)
+                {
+                    Console.WriteLine("Enter text:");
+                    var messege = Console.ReadLine();
+                    var buffer = Encoding.ASCII.GetBytes(messege);
+                    _socket.Send(buffer);
+                }
+            });
         }
     }
 }
