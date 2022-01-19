87ab159d9529a08ecc7ef5ca3090f492add28835
Yonatan Karby <jokerkarby@gmail.com>
2022-01-19 15:12:08 +0200

    add client socket class and interface

---
+++ PingPongExercise/Client/ClientSocket.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class ClientSocket : ITcpClient
+    {
+        public Task BeginWrite()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
